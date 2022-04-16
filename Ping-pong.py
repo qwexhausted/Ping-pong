e27170685177db05db0c9328a8d7e4676000ce4f
qwexhausted <102855492+qwexhausted@users.noreply.github.com>
2022-04-16 13:23:52 +0300

    Add files via upload

--- Ping-pong.py
+++ Ping-pong.py
@@ -18,21 +18,21 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def updatel(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_s] and self.rect.y >0:
-            self.rect.y-=self.speed
-        if keys_pressed[K_w]and self.rect.x<435:
+        if keys_pressed[K_s] and self.rect.y<435:
             self.rect.y+=self.speed
+        if keys_pressed[K_w]and self.rect.y >0:
+            self.rect.y-=self.speed
     def updater(self):
         keys_pressed = key.get_pressed()
-        if keys_pressed[K_DOWN] and self.rect.y >0:
-            self.rect.y-=self.speed
-        if keys_pressed[K_UP]and self.rect.y<435:
+        if keys_pressed[K_DOWN] and self.rect.y<435:
             self.rect.y+=self.speed
+        if keys_pressed[K_UP]and self.rect.y >0:
+            self.rect.y-=self.speed
 FPS = 60
 
-racketl= Player('racka.png',100,185,65,100,5)
-racketr= Player('racka.png',600,185,65,100,5)
-myach = GameSprite('myach.png',285,185,30,30,5)
+racket1= Player('racka1.png',0,185,100,160,7)
+racket2= Player('racka2.png',600,185,100,160,7)
+ball = GameSprite('myach.png',285,185,50,50,5)
 #создай окно игры
 window = display.set_mode((700,500))
 display.set_caption('Ping-pong')
@@ -42,13 +42,36 @@ game = True
 clock = time.Clock()
 score = 0
 keys_pressed = key.get_pressed()
-
-
-
+finish = False
+speed_x=5
+speed_y=5
+font.init()
+font1 = font.Font(None,55)
+lose1 = font1.render('PLAYER 1 LOSE!',True,(180,0,0))
+lose2 = font1.render('PLAYER 2 LOSE!',True,(180,0,0))
 while game:
     for e in event.get():
         if e.type == QUIT:
             game=False
-   
+    if finish!=True:
+        window.blit(background,(0,0))
+        racket1.reset()
+        racket2.reset()
+        racket1.updatel()
+        racket2.updater()
+        ball.reset()
+        
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y > 450 or ball.rect.y < 50:
+            speed_y *= -1
+        if sprite.collide_rect(racket1,ball) or sprite.collide_rect(racket2,ball):
+            speed_x*=-1
+        if ball.rect.x == 0:
+            window.blit(lose1, (200,200))
+            finish = True
+        if ball.rect.x == 770:
+            window.blit(lose2, (200,200))
+            finish = True
     clock.tick(FPS)
     display.update()
